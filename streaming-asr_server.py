8f744d77d2684ff8fcd24afbc5696c080109a7f7
josep crego <josep.crego@systrangroup.com>
2023-09-06 12:21:21 +0200

    light

--- streaming-asr_server.py
+++ streaming-asr_server.py
@@ -8,7 +8,7 @@ from python.StreamASR import StreamASR
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='This script launches an ASR (Whisper) server behind a REST api.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    group_model = parser.add_argument_group("Model`")
+    group_model = parser.add_argument_group("Whisper model`")
     group_model.add_argument('--model_size', type=str, help='Model size (tiny.en, tiny, base.en, base, small.en, small, medium.en, medium, large-v1, large-v2)', default='tiny')
     group_model.add_argument('--device', type=str, help='Device: cpu, cuda, auto', default='auto')
     group_model.add_argument('--compute_type', type=str, help='Compute type', default='int8')
