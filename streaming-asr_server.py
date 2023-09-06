432a7693e9d894e3b854112f090aa673fd42101c
josep crego <josep.crego@systrangroup.com>
2023-09-06 11:30:36 +0200

    light

--- streaming-asr_server.py
+++ streaming-asr_server.py
@@ -1,5 +1,6 @@
 import logging
 import argparse
+from datetime import datetime
 from faster_whisper import WhisperModel
 from flask import Flask, request, jsonify
 from python.StreamASR import StreamASR
@@ -11,9 +12,9 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, help='Device: cpu, cuda, auto', default='auto')
     parser.add_argument('--compute_type', type=str, help='Compute type', default='int8')
     parser.add_argument('--port', type=int, help='Port used in local server', default=5000)
-    parser.add_argument('--host', type=int, help='Host used (use 0.0.0.0 to allow distant access, otherwise use 127.0.0.1)', default='0.0.0.0')
+    parser.add_argument('--host', type=str, help='Host used (use 0.0.0.0 to allow distant access, otherwise use 127.0.0.1)', default='0.0.0.0')
     args = parser.parse_args()
-    logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=getattr(logging, 'INFO', None), filename=None)
+    logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=getattr(logging, 'INFO', None), filename='./log.{}'.format(datetime.now().strftime("%Y-%m-%d_%H:%M:%S")))
     asr = StreamASR(args.model_size, args.device, args.compute_type)
 
     app = Flask(__name__)
@@ -22,10 +23,10 @@ if __name__ == '__main__':
         content = request.json  # Assuming the client sends JSON data
         response = { 
             'transcript': asr( 
-                content['audio'], 
-                content['language'], 
-                content['history'] , 
-                int(content['beam_size']), 
+                content['audio'],
+                content['language'],
+                content['history'],
+                int(content['beam_size']),
                 content['task'])
             }
         return jsonify(response)
